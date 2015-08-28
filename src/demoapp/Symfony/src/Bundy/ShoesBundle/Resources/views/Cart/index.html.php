2d970f38d2a52ce6af8cc35a6162a88a66ac4f4e
Jeff Morgan <jeff.morgan@appdynamics.com>
2015-08-28 15:47:03 +0000

    Removed src/

--- src/demoapp/Symfony/src/Bundy/ShoesBundle/Resources/views/Cart/index.html.php
+++
@@ -1,9 +0,0 @@
-<?php $view->extend('BundyShoesBundle::layout.html.php'); ?>
-
-<h3>Your Cart</h3>
-<ul>
-<?php foreach ($items as $item) { ?>
-    <li><?php echo $view->escape($item->getProduct()->getName()); ?></li>
-<?php } ?>
-</ul>
-<a href="/cart/checkout">Checkout</a>
--- src/demoapp/Symfony/src/Bundy/ShoesBundle/Resources/views/Product/view.html.php
+++
@@ -1,8 +0,0 @@
-<?php $view->extend('BundyShoesBundle::layout.html.php'); ?>
-
-Product Name : <b><?php echo $view->escape($product->getName()); ?></b>
-
-<form action="/cart/addToCart" method="post">
-    <input type="hidden" name="productId" value="<?php echo $view->escape($product->getId()); ?>" />
-    <input type="submit" value="Add to Cart" />
-</form>
