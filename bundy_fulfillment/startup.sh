29f28407b1a814b3382fcfef23dd89fd20075d49
Jeff Morgan <jeff.morgan@appdynamics.com>
2015-09-02 16:59:11 +0000

    Removed commented out lines to clean up code

--- bundy_fulfillment/startup.sh
+++ bundy_fulfillment/startup.sh
@@ -6,21 +6,15 @@
 source /appdynamics/env.sh
 
 # Complete App Agent config
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/Fulfillment-Node1/${FUL_NODE_NAME}/g' /appdynamics/runProxy"
 source /appdynamics/env.sh && sed -i "s/Fulfillment-Node1/${FUL_NODE_NAME}/g" /appdynamics/runProxy
 /appdynamics/appdynamics-php-agent/install.sh -i /etc/php5/mods-available -a customer1@${CTRLR_KEY} --ignore-permissions ${CONTROLLER} ${APPD_PORT} ${APP_NAME} ${FUL_TIER_NAME} ${FUL_NODE_NAME}
 mv /appdynamics/runProxy /appdynamics/appdynamics-php-agent/proxy/
 chmod 777 /appdynamics/appdynamics-php-agent/proxy/runProxy
-#chown -R appdynamics.appdynamics /appdynamics
 php5enmod appdynamics_agent
 
 # Start MachineAgent
-#su - appdynamics -c 'source /appdynamics/env.sh && /appdynamics/MachineAgent/startMachineAgent.sh'
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/Fulfillment-Node1/${FUL_NODE_NAME}/g' /appdynamics/MachineAgent/startMachineAgent.sh"
 source /appdynamics/env.sh && sed -i "s/Fulfillment-Node1/${FUL_NODE_NAME}/g" /appdynamics/MachineAgent/startMachineAgent.sh
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/AGENT_OPTIONS -Dappdynamics.agent.uniqueHostId=/AGENT_OPTIONS -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.accountName=customer1 -Dappdynamics.agent.accountAccessKey=${CTRLR_KEY} -Dappdynamics.agent.uniqueHostId=/g' /appdynamics/MachineAgent/startMachineAgent.sh"
 source /appdynamics/env.sh && sed -i "s/AGENT_OPTIONS -Dappdynamics.agent.uniqueHostId=/AGENT_OPTIONS -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.accountName=customer1 -Dappdynamics.agent.accountAccessKey=${CTRLR_KEY} -Dappdynamics.agent.uniqueHostId=/g" /appdynamics/MachineAgent/startMachineAgent.sh
-#su - appdynamics -c 'source /appdynamics/MachineAgent/startMachineAgent.sh'
 source /appdynamics/MachineAgent/startMachineAgent.sh
 
 # Start Services
--- bundy_webserver/startup.sh
+++ bundy_webserver/startup.sh
@@ -15,29 +15,22 @@ sudo echo "54.190.170.102 cdn.bundyshoes.com" >> /etc/hosts
 source /appdynamics/env.sh
 
 # Complete App Agent config
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/Commerce-Node1/${COM_NODE_NAME}/g' /appdynamics/runProxy"
 source /appdynamics/env.sh && sed -i "s/Commerce-Node1/${COM_NODE_NAME}/g" /appdynamics/runProxy
 /appdynamics/appdynamics-php-agent/install.sh -i /etc/php5/mods-available -a customer1@${CTRLR_KEY} --ignore-permissions ${CONTROLLER} ${APPD_PORT} ${APP_NAME} ${COM_TIER_NAME} ${COM_NODE_NAME}
 mv /appdynamics/runProxy /appdynamics/appdynamics-php-agent/proxy/
 chmod 777 /appdynamics/appdynamics-php-agent/proxy/runProxy
-#chown -R appdynamics.appdynamics /appdynamics
 php5enmod appdynamics_agent
 echo "agent.proxy_output = /tmp/proxyout" >> /etc/php5/apache2/conf.d/20-appdynamics_agent.ini
 
 # Input EUM Key in parameters.yml
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i '/eum_key/c\      eum_key:           ${EUM_KEY}' /var/www/html/demoapp/Symfony/app/config/parameters.yml"
 source /appdynamics/env.sh && sed -i "/eum_key/c\      eum_key:           ${EUM_KEY}" /var/www/html/demoapp/Symfony/app/config/parameters.yml
 
 # Start MachineAgent
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/Commerce-Node1/${COM_NODE_NAME}/g' /appdynamics/MachineAgent/startMachineAgent.sh"
 source /appdynamics/env.sh && sed -i "s/Commerce-Node1/${COM_NODE_NAME}/g" /appdynamics/MachineAgent/startMachineAgent.sh
-#su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/AGENT_OPTIONS -Dappdynamics.agent.uniqueHostId=/AGENT_OPTIONS -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.accountName=customer1 -Dappdynamics.agent.accountAccessKey=${CTRLR_KEY} -Dappdynamics.agent.uniqueHostId=/g' /appdynamics/MachineAgent/startMachineAgent.sh"
 source /appdynamics/env.sh && sed -i "s/AGENT_OPTIONS -Dappdynamics.agent.uniqueHostId=/AGENT_OPTIONS -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.accountName=customer1 -Dappdynamics.agent.accountAccessKey=${CTRLR_KEY} -Dappdynamics.agent.uniqueHostId=/g" /appdynamics/MachineAgent/startMachineAgent.sh
 source /appdynamics/MachineAgent/startMachineAgent.sh
-#su - appdynamics -c 'source /appdynamics/MachineAgent/startMachineAgent.sh'
 
 # Set crontab
-#su - appdynamics -c 'crontab /appdynamics/cron.conf'
 crontab /appdynamics/cron.conf
 
 # Start services
